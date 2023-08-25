42c3957526ba1319db056f0ee0739e93207cb56b
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2023-08-25 11:39:30 -0700

    prettify code

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
     const className = document.body.className;
-    if(className == "light-theme") {
+    if (className == "light-theme") {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
